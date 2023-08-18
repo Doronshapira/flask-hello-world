f9aca5d25ca86067fb899548f1b0090644b7accc
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-08-18 18:06:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify,request
+from main import get_timing
 
+
+app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    print('go')
+    return 'I am alive'
+
+# Sample data
+data = [
+    {"id": 1, "name": "Item 1"},
+    {"id": 2, "name": "Item 2"}
+]
+
+@app.route('/flights_api/<flight_number>', methods=['GET'])
+def get_items(flight_number):
+    print('hi')
+    days = int(request.args.get('days', default=7))
+    return jsonify(get_timing(flight_number, days))
