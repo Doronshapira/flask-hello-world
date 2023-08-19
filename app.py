bb6d6772bb33d2ad5a9560ed4f6778f780d9f4fa
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-08-19 15:10:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def get_items(flight_number):
     return jsonify(get_timing(flight_number, days))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
