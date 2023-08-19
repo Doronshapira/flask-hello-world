a81a883d52d05c485b5c8c7749bdf794f65a3818
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-08-19 15:56:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_timing(flight_num,days_back):
     chrome_options.add_argument("disable-infobars")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
                  'Chrome/92.0.4515.159 Safari/537.36 '
     chrome_options.add_argument(f'user-agent={user_agent}')
