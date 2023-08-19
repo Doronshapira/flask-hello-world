d7a8671c57d234b365a504c12aac7f6643ad00b5
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-08-19 14:59:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,11 @@ def home():
     print('go')
     return 'I am alive'
 
-# Sample data
-data = [
-    {"id": 1, "name": "Item 1"},
-    {"id": 2, "name": "Item 2"}
-]
-
 @app.route('/flights_api/<flight_number>', methods=['GET'])
 def get_items(flight_number):
     print('hi')
     days = int(request.args.get('days', default=7))
     return jsonify(get_timing(flight_number, days))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
