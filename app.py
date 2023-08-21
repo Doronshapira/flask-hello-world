92526d56e859d845dabae5e207cf806437e6715d
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-08-21 13:54:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_sslify import SSLify
 
 app = Flask(__name__)
-CORS(app, origins=["www.flightiming.com"], supports_credentials=True)
+CORS(app, origins=["www.flightiming.com","https://4a7abb7c-2293-4673-bbd3-7390068b6773.dev.wix-code.com"], supports_credentials=True)
 
 @app.route('/')
 def home():
