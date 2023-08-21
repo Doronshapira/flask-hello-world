01b58c00594fd4d4f3cde0055225ea318b646c3f
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-08-21 16:03:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_timing(flight_num,days_back):
         arrival_airport =   df['Arrival Airport','Arrival Airport'].iloc[0]
         days_back=int(days_back)
         # Drop the first row if it contains any null values
-        df = df.iloc[1:].dropna()
+        df = df.dropna()
         print(df)
         df = df.head(days_back)
         num_delays=6
